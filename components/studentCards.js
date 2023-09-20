903565d00f4eb6bd2cb09c700dbb41c3b64584f7
Rui Shan <ruishan.teo03@gmail.com>
2023-09-20 09:50:14 +0800

    Add student pages

---
+++ components/studentCards.js
@@ -0,0 +1,35 @@
+class StudentCard extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <link rel="stylesheet" href="/css/styles.css" />
+    <link rel="stylesheet" href="/css/card.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <div class="card">
+      <div class="subclass">
+        <h4><b>PhD Booboo</b></h4>
+        <button class="icon-button">
+          <i class="fa fa-trash"></i>
+        </button>
+      </div>
+
+        <p>P5_Science</p>
+        <p>P5_Math</p>
+        <p>P5_English</p>
+    </div>
+        `;
+
+    this.addEventListener("click", (event) => {
+      event.stopPropagation();
+      window.location.href = "/pages/mainPages/detailedStudents.html";
+    });
+  }
+}
+
+customElements.define("student-card", StudentCard);
