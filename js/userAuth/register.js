9058c6f8b4cb1eb79b2de9aefa09149c6918f9a2
Rui Shan <ruishan.teo03@gmail.com>
2023-10-02 00:06:26 +0800

    Refactor userAuth

--- js/userAuth/register.js
+++ js/userAuth/register.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const registerForm = document.getElementById("register-form");
 
-  const handleLogin = async (event) => {
+  const handleRegister = async (event) => {
     event.preventDefault();
 
     const centreName = document.getElementById("centre-name").value;
@@ -11,45 +11,22 @@ document.addEventListener("DOMContentLoaded", function () {
     const confirmPassword = document.getElementById("confirm-password").value;
     const contact = document.getElementById("contact").value;
 
-    const data = {
+    if (password != confirmPassword) {
+      return showStatus("Passwords do not match", "error");
+    }
+
+    const args = getArgs();
+
+    args.updateType = "register";
+    args.updateBody = {
       centreName: centreName,
       fullName: fullName,
       email: email,
       password: password,
       contact: contact,
     };
-
-    if (password != confirmPassword) {
-      return showStatus("Passwords do not match", "error");
-    }
-
-    try {
-      const response = await fetch(
-        "http://localhost:8000/public/auth/register",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-        }
-      );
-
-      const result = await response.json();
-
-      if (result.message === "User registered successfully") {
-        showStatus("Successfully registered. Welcome", "success");
-        localStorage.setItem("accessToken", result.tokens.accessToken);
-        localStorage.setItem("user", JSON.stringify(result.user));
-
-        window.location.href = getHomePage(result.user);
-      } else {
-        showStatus("Registration failed: " + result.message, "error");
-      }
-    } catch (error) {
-      showStatus("Registration failed: " + error, "error");
-    }
+    authWorker.postMessage(args);
   };
 
-  registerForm.addEventListener("submit", handleLogin);
+  registerForm.addEventListener("submit", handleRegister);
 });
