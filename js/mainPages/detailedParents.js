c6166582817c50301815ac622cc2ec9278b86cf6
Rui Shan <ruishan.teo03@gmail.com>
2023-10-03 08:19:42 +0800

    Add parents landing page

---
+++ js/mainPages/detailedParents.js
@@ -0,0 +1,26 @@
+const urlParams = new URLSearchParams(window.location.search);
+const parentId = parseInt(urlParams.get("parentId"));
+
+const studentsWorker = new Worker("/js/workers/studentsWorker.js");
+
+const reloadStudents = () => {
+  const args = getArgs();
+  args.updateType = "";
+  args.parentId = parentId;
+  studentsWorker.postMessage(args);
+};
+
+{
+  studentsWorker.addEventListener("message", function (e) {
+    handleNotifications(e);
+
+    if (e.data.students) {
+      renderParent(getArgs());
+      renderStudents(e.data.students, args, null, null);
+    }
+  });
+}
+
+addCallback((args) => {
+  reloadStudents();
+});
